000c5383cfe3ffb8a26538990b78a1d0d65f70ff
Richard Barrett-Small <rickbs@gmail.com>
2014-07-01 10:05:33 +0100

    Sharing secret key oops

--- coloursite/settings_webfaction.py
+++ coloursite/settings_webfaction.py
@@ -5,3 +5,7 @@ BASE_DIR = os.path.dirname(__file__)
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '4v_ti^y#9&b^)nz9epc%in6%7swg_0^wngs1u%@shhz)00ck7q'
+
