88ed908d0313185875ee2ec3a2203b55460e837c
Rich Barrett-Small <rickbs@gmail.com>
2023-01-04 13:25:24 +0000

    from iomart

---
+++ coloursite/settings_gondolin.py
@@ -0,0 +1,10 @@
+# Django settings for coloursite project.
+from settings import *
+
+import os
+BASE_DIR = os.path.dirname(__file__)
+
+DEBUG = False 
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = ['colourlens.org', 'www.colourlens.org']
